d807daf1880edd63a329f4f5d177ac0300d2d3a3
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-06-21 15:17:37 -0500

    HelloWorld class added

---
+++ src/HelloWorldJava.java
@@ -0,0 +1,9 @@
+public class HelloWorldJava {
+    
+    public static void main(String[] args){
+//        Strings are only defined with double quotes in java
+//        single quotes are used for character 'a' but 'aa' doesn't work
+//        sout is the console log shortcut
+        System.out.println("Hello World");
+    }
+}
