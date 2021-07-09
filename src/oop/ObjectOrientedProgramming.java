c7d73ae4cd9ff01416d6570bc604ec8ebf0e7ed3
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-07-09 16:33:43 -0500

    oop

---
+++ src/oop/ObjectOrientedProgramming.java
@@ -0,0 +1,46 @@
+package oop;
+
+// anyone who can access
+public class ObjectOrientedProgramming {
+    public static void main(String[] args) {
+//        FIRST EXAMPLE
+//        // tell me about a person
+//        Person person = new Person("Laura", 30, "short", "whoKnows");
+////         this will not be visible if the name property is set to
+////         private in the Person class. (person file)
+////        person.name;
+//        System.out.println(person);
+
+//        SECOND EXAMPLE
+//        Person personTwo = new Person();
+//        personTwo.setNameOfPerson("LauraTwo");
+//        // this returns the name of this instance of the class and
+//        // returns a copy of the name
+//        System.out.println(personTwo.getNameOfPerson());
+        
+        
+        //    THIRD EXAMPLE
+        Person personThree = new Person();
+        personThree.setNameOfPerson("Laura");
+        System.out.println(personThree.getEntertainment("ride a roller " +
+                                                                "coaster"));
+        
+        Person personFour = new Person();
+        personFour.setNameOfPerson("Tiff");
+        System.out.println(personFour.getEntertainment("go tubing the " +
+                                                               "river"));
+        
+        
+        // WARMUP
+        
+        // TODO: CREATE A NEW "OBERON" CLASS WITH AT LEAST 6 PROPERTIES
+        //  THAT DESCRIBE OUR OBERON COHORT. MAKE SURE TO INCLUDE
+        //  AT LEAST THREE DIFFERENT TYPES OF DATA IN YOUR CLASS. IN A
+        //  SEPARATE FILE, CREATE 3 INSTANCES OF THE OBERON CLASS AND
+        //  PRINT OUT A STRING THAT USES AT LEAST 3 OF THOSE PROPERTIES
+        //  TO DESCRIBE THAT OBERON INSTANCE.
+        
+        
+        
+    }
+}
---
+++ src/oop/Person.java
@@ -0,0 +1,109 @@
+package oop;
+
+// public - anyone who has access to the oop package can see this
+public class Person {
+    //    Field - actual objects and primitives that help describe the
+//    characteristics of a class. just data nothing more nothing less.
+//    fields are private outside of this class. nothing can directly
+//    access this field
+//    every field gets an access modifier - changes the visibility of
+//    something
+    private String nameOfPerson;
+    private int ageOfPerson;
+    private String heightOfPerson;
+    private String nationalityOfPerson;
+    // static -> makes this variable constant with every instance of
+    // this class
+    private static final boolean hasSkin = true;
+    
+    //    Constructor - a special method in a class that tells java to make
+//    a class of this class (allows us to create an instance of a Person
+//    class). constructors have to be named the same of the class
+    // because this is set to public i can inject values into this
+// person
+    public Person(String nameOfPerson, int ageOfPerson,
+            String heightOfPerson, String nationalityOfPerson) {
+        // inject these values into the fields
+        // this - refers to the Person class created at the top of
+        // this file
+        // this.nameOfProperty = is being set equal to whatever gets
+        // passed into the method as arguments (see Object Oriented
+        // Programming File)
+        this.nameOfPerson = nameOfPerson;
+        this.ageOfPerson = ageOfPerson;
+        this.heightOfPerson = heightOfPerson;
+        this.nationalityOfPerson = nationalityOfPerson;
+    }
+    
+    // this empty constructor is needed if we want to make an
+    // instance of the Person class without any 'injectables'
+    // (potential arguments)
+    public Person(){
+    }
+
+
+
+//    Properties - methods on the classes that help us execute behavior
+//    of an object
+    
+    // command + n
+    // click getter and setter
+    // these are going to create 'getter' and 'setter' properties on the
+    // class for the properties we choose
+    //
+    
+    // since original nameOfPerson is set to private I can't access it
+    // in an instance of this class but since I am setting a public
+    // method then anyone can get/set the nameOfPerson - this allows
+    // people to get the name
+    public String getNameOfPerson() {
+        return nameOfPerson;
+    }
+    
+    // this allows others to set or re-assign the name
+    public void setNameOfPerson(String nameOfPerson) {
+        // this - means this instance of the person (from wherever
+        // setNameOfPerson is being called from
+        this.nameOfPerson = nameOfPerson;
+    }
+    
+    public int getAgeOfPerson() {
+        return ageOfPerson;
+    }
+    
+    public void setAgeOfPerson(int ageOfPerson) {
+        this.ageOfPerson = ageOfPerson;
+    }
+    
+    public String getHeightOfPerson() {
+        return heightOfPerson;
+    }
+    
+    public void setHeightOfPerson(String heightOfPerson) {
+        this.heightOfPerson = heightOfPerson;
+    }
+    
+    public String getNationalityOfPerson() {
+        return nationalityOfPerson;
+    }
+    
+    public void setNationalityOfPerson(String nationalityOfPerson) {
+        this.nationalityOfPerson = nationalityOfPerson;
+    }
+    
+    public static boolean isHasSkin() {
+        return hasSkin;
+    }
+    
+    //     unique class member that does this for us needs to be defined in
+//     oop
+    public String getEntertainment(String entertainment){
+
+//            return a string that will tell us what the person is doing
+        // ex: Laura is going to go ride a rollerCoaster
+        return this.nameOfPerson + " is going to go " + entertainment;
+    }
+    
+    
+    
+}
