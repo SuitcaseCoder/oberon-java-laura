84cbc943adca5562e7fe31025aaf13b09236fcd1
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-07-11 21:26:42 -0500

    PersonTwo

--- src/oop/PersonTwo.java
+++ src/oop/PersonTwo.java
@@ -1,5 +1,22 @@
 package oop;
 
 public class PersonTwo {
+    // instance variables:
+    public String firstName;
+    public String lastName;
     
+    // instance method
+    public String sayHello() {
+        return String.format("Hello from %s %s!", firstName, lastName);
+    }
+    
+    public static void main(String[] args) {
+        // create an instance of a PersonTwo
+        PersonTwo rick = new PersonTwo();
+        
+        rick.firstName = "Rick";
+        rick.lastName = "Sanchez";
+        System.out.println(rick.sayHello());
+        // prints "Hello from Rick Sanchez!"
+    }
 }
