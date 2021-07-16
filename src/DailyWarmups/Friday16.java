b94e79c0aa34b72b260b9cbecb0c0dcd1cc0012d
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-07-16 16:53:15 -0500

    warmups

---
+++ src/DailyWarmups/Friday16.java
@@ -0,0 +1,14 @@
+package DailyWarmups;
+
+public class Friday16 {
+    // TODO: Write a method named getTwentyPrimes() that returns a string
+    //  containing the first 20 prime numbers, each separated by a comma.
+    //  Output: "1, 2, 3, 5, 7, 11, 13, 17, 19"... until we have a total
+    //  count of 20 primes in the string.
+    
+    // Create an array list
+    // use a loop
+    // iterate through it
+    
+    
+}
---
+++ src/DailyWarmups/Jul15.java
@@ -0,0 +1,6 @@
+package DailyWarmups;
+
+public class Jul15 {
+    // basic classes:
+    //app , init - to kick things off, contacts,
+}
